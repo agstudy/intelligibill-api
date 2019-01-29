8bdf6986e0e4031f49cd297220bbf9340c398c28
agstudy <contact@ag-study.com>
2019-01-29 01:10:48 +0100

    reprice with solar pv

--- myswitch.py
+++ myswitch.py
@@ -15,7 +15,6 @@ import uuid
 import io
 import os
 
-
 local = False
 from flask_cors import CORS
 from datetime import datetime
@@ -28,55 +27,85 @@ tracker_table = dynamodb.Table('tracker_table')
 
 BILLS_BUCKET = "myswitch-bills-bucket"
 
+
 def user_id():
     return request.headers.get('user_id')
 
 
-
-
 def bill_id(priced):
-    return(f"""{priced["to_date"].replace("/","-")}_{priced["users_nmi"]}""")
+    return (f"""{priced["to_date"].replace("/","-")}_{priced["users_nmi"]}""")
+
 
 def bill_file_name(priced):
     return f"private/{user_id()}/{bill_id(priced)}.pdf"
 
 
-def populate_tracking(bests,priced,nb_offers,key_file):
+def populate_tracking(bests, priced, nb_offers, key_file):
+    spot_date = datetime.today().strftime("%Y-%m-%d-%H-%M-%S")
+    customer_id = user_id()
 
-     to_day = datetime.today().strftime("%Y-%m-%d")
-     customer_id = user_id()
+    saving = -1;
+    if len(bests):
+        saving = bests[0]["saving"]
+    tracking = {
+        'avg_price': priced["avg"],
+        'ranking': f"{len(bests)}/{nb_offers}",
+        'saving': saving,
+        'to_date': priced["to_date"]
+    }
 
-     saving = -1;
-     if len(bests) :
-          saving = bests[0]["saving"]
-     tracking = {
-         'avg_price' : priced["avg"],
-         'ranking'  :  f"{len(bests)}/{nb_offers}",
-         'saving'   : saving,
-         'to_date'  :priced["to_date"]
-     }
-
-     item = {
-       'customer_id': customer_id,
-       'source_bill':{'url' :key_file},
-       'to_date_bill_id':bill_id(priced),
-       'spot_date': to_day,
-       'bests': bests,
-       'priced':priced,
-        'tracking':tracking
-     }
-     item = json.loads(json.dumps(item), parse_float=decimal.Decimal)
-     if customer_id:
-         tracker_table.put_item(Item=item)
+    item = {
+        'customer_id': customer_id,
+        'source_bill': {'url': key_file},
+        'to_date_bill_id': bill_id(priced),
+        'spot_date': spot_date,
+        'bests': bests,
+        'priced': priced,
+        'tracking': tracking
+    }
+    item = json.loads(json.dumps(item), parse_float=decimal.Decimal)
+    if customer_id:
+        tracker_table.put_item(Item=item)
 
 
-def bad_results(message, priced = {}):
-    return  jsonify(
-            { 'bests': [],
-              'evaluated' : -1,
-              'bill': priced,
-              "message":message
-            }), 200
+# TODO: finish this
+def update_tracking(bests, priced, nb_offers):
+    spot_date = datetime.today().strftime("%Y-%m-%d-%H-%M-%S")
+    customer_id = user_id()
+
+    saving = -1;
+    if len(bests):
+        saving = bests[0]["saving"]
+    tracking = {
+        'avg_price': priced["avg"],
+        'ranking': f"{len(bests)}/{nb_offers}",
+        'saving': saving,
+        'to_date': priced["to_date"]
+    }
+
+    priced = json.loads(json.dumps(priced), parse_float=decimal.Decimal)
+    bests = json.loads(json.dumps(bests), parse_float=decimal.Decimal)
+    tracking = json.loads(json.dumps(tracking), parse_float=decimal.Decimal)
+    key = {
+        "to_date_bill_id": bill_id(priced),
+        "customer_id": customer_id}
+    tracker_table.update_item(
+        Key=key,
+        UpdateExpression="set priced=:priced,tracking=:tracking,best=:bests,spot_date=:spot_date",
+        ExpressionAttributeValues={':priced': priced,
+                                   ':bests': bests,
+                                   ':tracking': tracking,
+                                   ':spot_date': spot_date},
+        ReturnValues="UPDATED_NEW")
+
+
+def bad_results(message, priced={}):
+    return jsonify(
+        {'bests': [],
+         'evaluated': -1,
+         'bill': priced,
+         "message": message
+         }), 200
 
 
 @app.route('/bill/page', methods=['GET'])
@@ -90,10 +119,10 @@ def bill_source():
     s3_resource.Bucket(BILLS_BUCKET).download_file(Filename=file_name, Key=key_image)
     with open(file_name, 'rb') as bites:
         return send_file(
-                     io.BytesIO(bites.read()),
-                     attachment_filename=key_image,
-                     mimetype='image/jpg'
-               )
+            io.BytesIO(bites.read()),
+            attachment_filename=key_image,
+            mimetype='image/jpg'
+        )
 
 
 @app.route('/bill/pdf', methods=['GET'])
@@ -104,10 +133,10 @@ def bill_pdf():
     s3_resource.Bucket(BILLS_BUCKET).download_file(Filename=file_name, Key=key)
     with open(file_name, 'rb') as bites:
         return send_file(
-                     io.BytesIO(bites.read()),
-                     attachment_filename=bill_url,
-                     mimetype='application/pdf'
-               )
+            io.BytesIO(bites.read()),
+            attachment_filename=bill_url,
+            mimetype='application/pdf'
+        )
 
 
 @app.route("/parse", methods=["POST"])
@@ -115,45 +144,42 @@ def parse():
     file_obj = request.files.get("pdf")
     pdf_data = file_obj.read()
     file_name = file_obj.filename
-    print("trying to parse ...",file_name)
+    print("trying to parse ...", file_name)
     id = f"/tmp/{uuid.uuid1()}.pdf"
-    with NamedTemporaryFile("wb",suffix=".pdf",delete=False) as out:
+    with NamedTemporaryFile("wb", suffix=".pdf", delete=False) as out:
         out.write(pdf_data)
-        is_bill, message =  Extractor.check_bill(out.name)
+        is_bill, message = Extractor.check_bill(out.name)
         if not is_bill:
-            return  bad_results(message)
+            return bad_results(message)
 
         out.write(pdf_data)
         copyfile(out.name, id)
         Extractor.process_pdf(out.name)
-        bp = BillParser(xml_=Extractor.xml_,xml_data_ =Extractor.xml_data, txt_=Extractor.txt_,file_name=file_name)
+        bp = BillParser(xml_=Extractor.xml_, xml_data_=Extractor.xml_data, txt_=Extractor.txt_, file_name=file_name)
         bp.parse_bill()
         if not bp.parser:
             return bad_results("no parsing")
         parsed = bp.parser.json
 
         priced: dict = Bill(dict(parsed))()
-        res,nb_offers,nb_retailers = get_bests(priced,"",n=-1)
-        bests=[ x for x in res if x["saving"]>0]
+        res, nb_offers, nb_retailers = get_bests(priced, "", n=-1)
+        bests = [x for x in res if x["saving"] > 0]
 
         if not local:
             key_file = bill_file_name(priced)
-            populate_tracking(bests,priced,nb_offers, key_file = key_file)
+            populate_tracking(bests, priced, nb_offers, key_file=key_file)
             s3_resource.Bucket(BILLS_BUCKET).upload_file(Filename=id, Key=key_file)
             os.remove(id)
 
         if not len(bests):
-            return  bad_results("no saving",priced)
+            return bad_results("no saving", priced)
 
-
-        return  jsonify(
-            {"evaluated":nb_offers,
-             "nb_retailers":nb_retailers,
-             "bests":bests,
-             "bill":priced,
-             "message":"saving"}),200
-
-
+        return jsonify(
+            {"evaluated": nb_offers,
+             "nb_retailers": nb_retailers,
+             "bests": bests,
+             "bill": priced,
+             "message": "saving"}), 200
 
 
 # Helper class to convert a DynamoDB item to JSON.
@@ -167,8 +193,7 @@ class DecimalEncoder(json.JSONEncoder):
         return super(DecimalEncoder, self).default(o)
 
 
-
-@app.route("/tracker",methods=["GET"])
+@app.route("/tracker", methods=["GET"])
 def tracker():
     customer_id = user_id()
     try:
@@ -176,43 +201,63 @@ def tracker():
         items = response['Items']
         tracking = []
         for x in items:
-            item =x.get("tracking", None)
+            item = x.get("tracking", None)
             if item:
                 if "source_bill" in x:
                     bill_url = os.path.basename(x["source_bill"]["url"])
-                    nb_pages = len(x["source_bill"].get("images",[]))
-                    item.update({"nb_pages":nb_pages,"bill_url":bill_url})
+                    nb_pages = len(x["source_bill"].get("images", []))
+                    item.update({"nb_pages": nb_pages, "bill_url": bill_url})
                 tracking.append(item)
-        result = json.dumps(tracking , indent=4, cls=DecimalEncoder)
-        return  result, 200
+        result = json.dumps(tracking, indent=4, cls=DecimalEncoder)
+        return result, 200
     except ClientError as e:
         print(e.response['Error']['Message'])
         raise e
+
 
 @app.route("/check", methods=["POST"])
 def check():
     file_obj = request.files.get("pdf")
     pdf_data = file_obj.read()
     file_name = file_obj.filename
-    with NamedTemporaryFile("wb",suffix=".pdf",delete=False) as out:
+    with NamedTemporaryFile("wb", suffix=".pdf", delete=False) as out:
         out.write(pdf_data)
-        is_bill, message =  Extractor.check_bill(out.name)
+        is_bill, message = Extractor.check_bill(out.name)
         return jsonify(
-            {"is_bill":is_bill,
-             "message":message}
+            {"is_bill": is_bill,
+             "message": message}
         )
+
+
+@app.route("/reprice", methods=["POST"])
+def reprice():
+
+    parsed = request.get_json()
+    print("trying to reprice ...")
+    priced: dict = Bill(dict(parsed))()
+    res, nb_offers, nb_retailers = get_bests(priced, "", n=-1)
+    bests = [x for x in res if x["saving"] > 0]
+
+    if not local:
+        update_tracking(bests, priced, nb_offers)
+    if not len(bests):
+        return bad_results("no saving", priced)
+    return jsonify(
+        {"evaluated": nb_offers,
+         "nb_retailers": nb_retailers,
+         "bests": bests,
+         "bill": priced,
+         "message": "saving"}), 200
 
 
 # We only need this for local development.
 if __name__ == '__main__':
 
     import yaml
+
     json_data = open('zappa_settings.yaml')
     env_vars = yaml.load(json_data)['api']['environment_variables']
     for key, val in env_vars.items():
         os.environ[key] = val
 
-
-    app.run(port =2003,load_dotenv=True)
-
-
+    app.run(port=2003, load_dotenv=True)
